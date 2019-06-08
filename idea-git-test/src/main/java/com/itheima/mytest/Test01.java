9537e0b0eb65f90d883409fc4a7a06059f44da0c
wangpengfei <564224447@qq.com>
2019-06-08 19:26:00 +0800

    新建项目

---
+++ idea-git-test/src/main/java/com/itheima/mytest/Test01.java
@@ -0,0 +1,8 @@
+package com.itheima.mytest;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello");
+    }
+}
